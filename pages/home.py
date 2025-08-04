a130f87a8b68c446ebc9b4c6c128aca4e9df3ac5
Innocent Wowa <61548006+innowowa@users.noreply.github.com>
2025-08-04 10:00:13 +0200

    updated files

--- pages/home.py
+++ pages/home.py
@@ -18,6 +18,7 @@ dash.register_page(__name__, path="/home")
 data = load_stored_data()
 min_date = pd.to_datetime(data['Date']).min()
 max_date = pd.to_datetime(data['Date']).max()
+max_date = datetime.today()
 
 def load_menu(filtered):
 
@@ -52,8 +53,8 @@ layout = html.Div([
                     min_date_allowed=min_date,
                     max_date_allowed=max_date,
                     initial_visible_month=max_date,
-                    # start_date=max_date - pd.Timedelta(days=1),
-                    start_date=min_date,
+                    start_date=max_date - pd.Timedelta(days=7),
+                    # start_date=min_date,
                     end_date=max_date,
                     display_format='YYYY-MM-DD',
                 )
@@ -208,61 +209,63 @@ layout = html.Div([
      ]
 )
 def update_dashboard(urlparams, start_date, end_date, program, hf, age):
-    if urlparams['location'] == None:
-        raise PreventUpdate
+    # if urlparams['location'] == None:
+    #     raise PreventUpdate
     # if urlparams['location'] is None:
     #     html.Div("No URL parameters found", style={'color': 'red'})
     # search_url = data[data['Facility_CODE'].str.lower() == new_flag]
     # print(len(search_url))
 
-    if urlparams:
+    if urlparams['location']:
         print(urlparams['location'])
         search_url = data[data['Facility_CODE'].str.lower() == urlparams['location'].lower()]
+    else:
         search_url = data
-        filtered_data_date = search_url[(pd.to_datetime(search_url['Date']) >= pd.to_datetime(start_date)) & 
+    # search_url = data
+    filtered_data_date = search_url[(pd.to_datetime(search_url['Date']) >= pd.to_datetime(start_date)) & 
                             (pd.to_datetime(search_url['Date']) <= pd.to_datetime(end_date))]
         
-        mask = pd.Series(True, index=filtered_data_date.index)
-        if program:
-            mask &= (filtered_data_date['Program'] == program)
-        if hf:
-            mask &= (filtered_data_date['Facility'] == hf)
-        if age:
-            mask &= (filtered_data_date['Age_Group'] == age)
-        filtered_data = filtered_data_date[mask].copy()
-        print(len(filtered_data))
+    mask = pd.Series(True, index=filtered_data_date.index)
+    if program:
+        mask &= (filtered_data_date['Program'] == program)
+    if hf:
+        mask &= (filtered_data_date['Facility'] == hf)
+    if age:
+        mask &= (filtered_data_date['Age_Group'] == age)
+    filtered_data = filtered_data_date[mask].copy()
+    print(len(filtered_data))
 
 
         
         # Update counts
-        total_count = create_count(filtered_data,'encounter_id', 'Encounter', 'REGISTRATION')
-        male_count = create_count(filtered_data,'encounter_id', 'Gender', 'M', 'Encounter', 'REGISTRATION')
-        female_count = create_count(filtered_data,'encounter_id', 'Gender', 'F', 'Encounter', 'REGISTRATION')
-        over5_count = create_count(filtered_data,'encounter_id', 'Age_Group', 'Over 5', 'Encounter', 'REGISTRATION')
-        under5_count = create_count(filtered_data,'encounter_id', 'Age_Group', 'Under 5', 'Encounter', 'REGISTRATION')
+    total_count = create_count(filtered_data,'encounter_id', 'Encounter', 'REGISTRATION')
+    male_count = create_count(filtered_data,'encounter_id', 'Gender', 'M', 'Encounter', 'REGISTRATION')
+    female_count = create_count(filtered_data,'encounter_id', 'Gender', 'F', 'Encounter', 'REGISTRATION')
+    over5_count = create_count(filtered_data,'encounter_id', 'Age_Group', 'Over 5', 'Encounter', 'REGISTRATION')
+    under5_count = create_count(filtered_data,'encounter_id', 'Age_Group', 'Under 5', 'Encounter', 'REGISTRATION')
         
         # Update charts
-        enrollments_fig = create_column_chart(df=filtered_data, x_col='Program', y_col='person_id',
+    enrollments_fig = create_column_chart(df=filtered_data, x_col='Program', y_col='person_id',
                                         title='Program', x_title='Program Name', y_title='Number of Patients')
         
-        daily_visits_fig = create_line_chart(df=filtered_data, date_col='Date', y_col='person_id',
+    daily_visits_fig = create_line_chart(df=filtered_data, date_col='Date', y_col='person_id',
                                             title='Daily Patient Visits', x_title='Date', y_title='Number of Patients')
         
-        visit_type_fig = create_pie_chart(df=filtered_data, names_col='new_revisit', values_col='person_id',
+    visit_type_fig = create_pie_chart(df=filtered_data, names_col='new_revisit', values_col='person_id',
                                         title='Patient Visit Type')
         
-        age_dist_fig = create_age_gender_histogram(df=filtered_data, age_col='Age', gender_col='Gender',
+    age_dist_fig = create_age_gender_histogram(df=filtered_data, age_col='Age', gender_col='Gender',
                                                 title='Age Distribution', xtitle='Age', ytitle='Number of patients', bin_size=5)
         
-        complaints_fig = create_horizontal_bar_chart(df=filtered_data, label_col='obs_value_coded', value_col='person_id',
+    complaints_fig = create_horizontal_bar_chart(df=filtered_data, label_col='obs_value_coded', value_col='person_id',
                                                 title='Top 10 Presenting Complaints', x_title='Number of Patients', y_title='Presenting Complaint',
                                                 filter_col1='Encounter', filter_value1='PRESENTING COMPLAINTS', filter_col2='concept_name', filter_value2='Presenting complaint')
         
-        diagnoses_fig = create_horizontal_bar_chart(df=filtered_data, label_col='obs_value_coded', value_col='person_id',
+    diagnoses_fig = create_horizontal_bar_chart(df=filtered_data, label_col='obs_value_coded', value_col='person_id',
                                                 title='Top 10 Primary Diagnoses', x_title='Number of Patients', y_title='Presenting Complaint',
                                                 filter_col1='Encounter', filter_value1='OUTPATIENT DIAGNOSIS', filter_col2='concept_name', filter_value2='Primary diagnosis')
         
-        return (total_count, male_count, female_count, over5_count, under5_count,
+    return (total_count, male_count, female_count, over5_count, under5_count,
                 enrollments_fig,
                 daily_visits_fig, 
                 visit_type_fig, age_dist_fig,
