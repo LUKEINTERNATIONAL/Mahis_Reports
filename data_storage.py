05a58e0f82ba43c167f246a67e465f9609ff6d0a
Innocent Wowa <61548006+innowowa@users.noreply.github.com>
2025-08-21 15:37:32 +0200

    updated files

--- data_storage.py
+++ data_storage.py
@@ -1,22 +1,27 @@
-import pandas as pd
 import os
+import pandas as pd
 from config import DB_CONFIG_AWS_TEST, QERY_OPD_TEST, QERY_OPD_PROD
 from models import fetch_and_store_data
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 
 query = QERY_OPD_TEST
 
+# Read base dir from environment, fallback to current directory
+BASE_DIR = os.getenv("DASH_APP_DIR", os.getcwd())
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
 def main():
-    # Fetch data from the database and store it in a CSV file
     df = fetch_and_store_data(query)
-    path = os.getcwd()
-    data_dir = os.path.join(os.getcwd(), "data")
-    os.makedirs(data_dir, exist_ok=True)
-    # Load the stored data
-    loaded_df = pd.read_csv(f'{path}/data/latest_data_opd.csv')
-    print("Data loaded successfully to:", f'{path}/data/latest_data_opd.csv')
-    print(loaded_df.iloc[:,6].tail(10))
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+    csv_path = os.path.join(DATA_DIR, "latest_data_opd.csv")
+    # df.to_csv(csv_path, index=False)
+
+    loaded_df = pd.read_csv(csv_path)
+    print("Data saved successfully to:", csv_path)
+    print(loaded_df.iloc[:, 6].tail(10))
     print(len(loaded_df))
 
 if __name__ == "__main__":
