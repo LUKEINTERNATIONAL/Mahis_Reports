f9e55aef12cb7e56164d723efd0cf726549ec265
Innocent Wowa <61548006+innowowa@users.noreply.github.com>
2026-01-25 17:24:12 +0200

    dashboard_config

--- pages/home.py
+++ pages/home.py
@@ -231,6 +231,8 @@ def update_dashboard(gen_clicks, menu_clicks, interval, urlparams, start_date, e
     data_opd["DateValue"] = pd.to_datetime(data_opd[DATE_]).dt.date
     today = dt.today().date()
     data_opd["months"] = data_opd["DateValue"].apply(lambda d: (today - d).days // 30)
+    
+    data_opd = data_opd.dropna(subset = ['obs_value_coded','concept_name', 'Value','ValueN', 'DrugName', 'Value_name'], how='all')
     # data_opd.to_excel("data/archive/hmis.xlsx", index=False)
     
     # Filter by URL params (e.g. Facility Code)
