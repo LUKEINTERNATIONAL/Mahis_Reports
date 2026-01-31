608c4a05ab9fe9151e5f6fcfd9c8ffe1c578b8f3
Innocent Wowa <61548006+innowowa@users.noreply.github.com>
2026-01-31 08:42:53 +0200

    dashboard_config

--- start_prod.sh
+++ start_prod.sh
@@ -1,25 +1,3 @@
-# #!/bin/bash
-# set -e
-
-# # Activate virtual environment
-# echo "Activating virtual environment..."
-# source venv/bin/activate
-
-# # Start Gunicorn
-# echo "Starting Gunicorn..."
-# nohup python3 -m gunicorn \
-#     --workers 4 \
-#     --timeout 30 \
-#     --graceful-timeout 30 \
-#     --keep-alive 5 \
-#     --bind 0.0.0.0:8050 \
-#     wsgi:server \
-#     > gunicorn.log 2>&1 &
-
-# echo "Gunicorn started. Logs: gunicorn.log"
-
-
-# """
 #!/bin/bash
 set -e
 
@@ -39,6 +17,4 @@ exec python -m gunicorn \
     --access-logfile /home/ubuntu/Mahis_Reports/gunicorn_access.log \
     --error-logfile /home/ubuntu/Mahis_Reports/gunicorn_error.log \
     wsgi:server
-    
-# """
 
